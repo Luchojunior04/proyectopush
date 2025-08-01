8d360816194ec44ddc09ab516f24cf36a8702c4a
Ernesto56734856 <provisionalernesto@gmail.com>
2025-07-31 19:59:32 -0500

    return

---
+++ index.js
@@ -0,0 +1,35 @@
+const pregunta = prompt("elije un numero");
+
+if( pregunta == "5") {
+    alert('numero es 5')
+} else if('numero2')
+
+    switch (pregunta) {
+        case '2':
+            alert('numero 2')
+
+         case '5':
+            alert('numero 5')    
+    }
+
+    function nombreFuncion(parametros){}
+
+    function suma(a, b) {
+        alert(a + b)
+    }
+
+    function saludo(nombe){
+        alert('!Hola' + nombre + '!')
+    }
+
+    saludo(Angel)
+
+    suma(5, 2)
+
+    function suma(a, b){
+        alert(a + b);
+        return a + b
+    }
+
+    let resultado = suma(2, 2);
+    console.log(resultado)
